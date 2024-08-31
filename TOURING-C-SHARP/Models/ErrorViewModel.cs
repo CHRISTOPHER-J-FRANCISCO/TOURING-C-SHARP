d8ed8e9fe17f786c9830333c42620415e34105ab
Christopher <cjfg00gl3@gmail.com>
2024-08-31 13:53:16 -0400

    Initial commit: Created .NET MVC project with basic structure

---
+++ TOURING-C-SHARP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TOURING_C_SHARP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
